556f1ac7df8b4edf078273d72314f73602d64279
wanghuifeng1997 <1426455005@qq.com>
2021-08-17 15:06:50 +0800

    更改协议内容

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 /**
- * Copyright (c) [Year] [analysis-location]
- * [Software Name] is licensed under Mulan PSL v2.
+ * Copyright (c) [Year] [Wang hui feng]
+ * [analysis-location] is licensed under Mulan PSL v2.
  * You can use this software according to the terms and conditions of the Mulan PSL v2.
  * You may obtain a copy of Mulan PSL v2 at:
-         http://license.coscl.org.cn/MulanPSL2
+ *       http://license.coscl.org.cn/MulanPSL2
  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
  * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
  * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
