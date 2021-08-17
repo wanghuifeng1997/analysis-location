28f7ab4a89d04c23ee4ade2f4baaf1a5ebfc2a89
wanghuifeng1997 <1426455005@qq.com>
2021-08-17 14:51:58 +0800

    完善协议

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 /**
+ * Copyright (c) [Year] [analysis-location]
+ * [Software Name] is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+         http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+
+/**
  * 创建一个Url 类库解析Url 信息  */
 class analysisLocation {
   location = {
@@ -35,7 +48,8 @@ class analysisLocation {
     this._host();
     this._origin();
     this._host();
-  }
+    this._href();
+  };
   /* 数组合并 */
   /* arrayMerging(arr=[],sbol=""){
 
